62439fed99c5679aa4130acbd826c0cc0a62d6bf
ADKokalarov23 <ADKokalarov23@codingburgas.bg>
2025-06-11 21:17:26 +0300

    Add Markers

--- MapStaraZagora.js
+++ MapStaraZagora.js
@@ -5,3 +5,15 @@ var map = L.map('map', {
 L.tileLayer('https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=pIX3iqzKhMQUtNsChrFh', {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
 }).addTo(map);
+
+map.on('click', function (e) {
+    var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+    marker.bindPopup("You clicked at:<br>" + e.latlng.lat.toFixed(5) + ", " + e.latlng.lng.toFixed(5)).openPopup();
+
+    // Double-click the marker to remove it
+    marker.on('dblclick', function (event) {
+        // Prevent the map's default double-click behavior
+        event.originalEvent.stopPropagation();
+        map.removeLayer(marker);
+    });
+});
